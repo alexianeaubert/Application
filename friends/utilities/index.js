1e619ab1f3bb41e39215e05c65585fefcf2a13c1
alexianeaubert <alexianeaubert@outlook.com>
2018-03-27 15:30:27 +0200

    Affichage posts des amis + CSS

--- friends/utilities/index.js
+++ friends/utilities/index.js
@@ -16,27 +16,23 @@ function findUsers(req,res){
     Friend.find({'user':MonId}, function(err,users){
         //Le Friend au dessus c'est ma table
         //users en violet c'est le résultat de ma requête : c'est un tableau
-        console.log(users);
         if (err) throw err;
         users.forEach(function(couple){
-        //couple en violent entre parenthèse c'est un couple friend/user
+        //couple en violet entre parenthèse c'est un couple friend/user
             // couple.friend(Tableau.push);
             Tableau.push(couple.friend);
         });
 
             //Fonction qui va me permettre de me cacher et de cacher ceux que je ne suis pas
             // Faire une deuxième fonction pour récupérer ce que je follow déjà
-            User.find({ $and: [{'_id':{'$ne':MonId}}, {'_id':{'$in':Tableau}}]},function(err,users){
+            User.find({ $and: [{'_id':{'$ne':MonId}}, {'_id':{'$nin':Tableau}}]},function(err,users){
                 if (err) throw err;
-                console.log("user passé");
                 //$ne : négatif
                 //$in : permet de sélectioner tous les éléments qui sont dans mon tableau
                 //$nin : inverse de in
-                User.find({ $and: [{'_id':{'$ne':MonId}},{'_id':{'$nin':Tableau}}]},function(err,unUsers){
+                User.find({ $and: [{'_id':{'$ne':MonId}},{'_id':{'$in':Tableau}}]},function(err,unUsers){
                     if (err) throw err;
-                    console.log("unuser passé")
-                    res.render('friend', {unUsers:unUsers, users:users})
-                    console.log("tout c'est bien passé")
+                    res.render('friend', {users:users, unUsers:unUsers});
                 });
             });
     });
--- profil/utilities/index.js
+++ profil/utilities/index.js
@@ -4,12 +4,27 @@ function findPosts(req,res){
     //On cherche les users avec le username qui est rentré
     let Post = require('../../post/models/post');
     let id = mongoose.Types.ObjectId(req.session.user._id);
+    let Friend = require('../../friends/models/friends');
+    tableauAmi = [];
 
     Post.find({'user': id},function(err,posts){
         if (err) throw err;
-        res.render('profil', {nom : req.session.user.local.username, posts:posts});
-        
+
+        // res.render('profil', {nom : req.session.user.local.username, posts:posts});
+
+        Friend.find({'user':id}, function(err,amis){
+            if (err) throw err;
+            amis.forEach(function(ami){
+                tableauAmi.push(ami.friend);
+            });
+
+            Post.find({'user':{'$in':tableauAmi}}).populate('user').exec(function(err, postsAmi){
+                if (err) throw err;
+                res.render('profil', {nom: req.session.user.local.username, posts: posts, postsAmi:postsAmi});
+            });
+        });
     });
-}
+    
+};
   
 module.exports.findPosts = findPosts;
